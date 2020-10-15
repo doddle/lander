432184a914f156e7e615978706bd168d6e783ad7
David Stark <david@starkers.org>
2020-10-15 02:50:45 +0100

    wip

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.14
 
 require (
 	github.com/gofiber/fiber/v2 v2.0.6
-	github.com/jgimeno/go-identicon v0.0.0-20180119140643-9f6b47b0e3e2 // indirect
+	github.com/jgimeno/go-identicon v0.0.0-20180119140643-9f6b47b0e3e2
 	github.com/jinzhu/gorm v1.9.16
+	github.com/llgcode/draw2d v0.0.0-20200930101115-bfaf5d914d1e
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/withmandala/go-log v0.1.0
 	k8s.io/api v0.19.0
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/patrickmn/go-cache"
+	"github.com/starkers/lander/identicon"
 	"github.com/withmandala/go-log"
 )
 
@@ -41,9 +44,33 @@ func main() {
 		DisableStartupMessage: true,
 	}
 	app := fiber.New(fiberCfg)
-	// app.Get("/", func(c *fiber.Ctx) error {
-	// 	return c.SendString("hello")
-	// })
+	app.Get("/favicon.ico", func(c *fiber.Ctx) error {
+		name := func(c *fiber.Ctx) string {
+			if c.Hostname() == "" {
+				return "unknown"
+			}
+			// strip any trailing ports off and just return the hostname
+			return strings.Split(c.Hostname(), ":")[0]
+		}(c)
+
+		fileName := fmt.Sprintf("icon-%s.png", name)
+
+		if _, err := os.Stat(fileName); os.IsNotExist(err) {
+			icon := identicon.Generate([]byte(name))
+
+			f, err := os.Create(fileName)
+			if err != nil {
+				logger.Error(err)
+			}
+			err = icon.WriteImage(f)
+			if err != nil {
+				logger.Error(err)
+			}
+			f.Close()
+			logger.Infof("rendered a new icon for: %s", name)
+		}
+		return c.SendFile(fileName)
+	})
 	app.Get("/v1/endpoints", func(c *fiber.Ctx) error {
 		logger.Info("v1/endpoints")
 		// get ALL endpoints
