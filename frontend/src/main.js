2f686b848d8a0491c3be0f7c034fa45bad6c3b85
David Stark <david@starkers.org>
2020-10-19 23:43:08 +0100

    veutify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import crono from "vue-crono";
+import vuetify from "./plugins/vuetify";
 
 Vue.use(crono);
 Vue.config.productionTip = false;
@@ -11,5 +12,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,12 @@
-// when running locally, allow the frontend to proxy results to the golang API
-// No CORS so u can live test basically
-
 module.exports = {
   devServer: {
     proxy: {
-      '^/': {
-        target: 'http://localhost:8000',
+      "^/": {
+        target: "http://localhost:8000",
         ws: false,
         changeOrigin: true
       }
     }
-  }
-}
+  },
+  transpileDependencies: ["vuetify"]
+};
