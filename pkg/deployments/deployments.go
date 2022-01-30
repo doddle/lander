0662371dd43ccb871e7fc909a0cf84f70867b7c1
David Stark <david.stark@doddle.com>
2022-01-30 11:40:42 +0000

    see when deployments are being rolled out

--- pkg/deployments/deployments.go
+++ pkg/deployments/deployments.go
@@ -57,36 +57,52 @@ func AssembleDeploymentPieChart(
 	var resultColors []string
 	var resultLabels []string
 	var resultSeries []int64
-	var totalBad int64
-	var totalGood int64
+	var totalDown int64    // Conditions: anything else
+	var totalHealthy int64 // Conditions: Available + Progressing
+	var totalPending int64 // Conditions: Progressing (but containers available not 100%)
+
+	// colors: https://apexcharts.com/docs/options/colors/
+	// vs https://vuetifyjs.com/en/styles/colors/#material-colors
+	//
+	// try to stick to "$color lighten 2"
+	colDown := "#E57373"
+	colHealthy := "#81C784"
+	colPending := "#FFB74D"
 
 	data, err := getAllDeployments(logger, clientSet)
 	if err != nil {
 		logger.Error(err)
 	}
 	for _, deployment := range data.Items {
-		if isReady(deployment) {
-			totalGood++
+		if isHealthy(deployment) {
+			totalHealthy++
 		} else {
-			totalBad++
+			if isPending(deployment) {
+				totalPending++
+			} else {
+				totalDown++
+			}
 		}
 	}
 
-	// colors: https://apexcharts.com/docs/options/colors/
-	// vs https://vuetifyjs.com/en/styles/colors/#material-colors
-	if totalBad > 0 {
-		resultLabels = append(resultLabels, "Errored")
-		resultSeries = append(resultSeries, totalBad)
-		resultColors = append(resultColors, "#E57373")
+	if totalDown > 0 {
+		resultLabels = append(resultLabels, "Down")
+		resultSeries = append(resultSeries, totalDown)
+		resultColors = append(resultColors, colDown)
 	}
-	if totalGood > 0 {
+	if totalHealthy > 0 {
 		resultLabels = append(resultLabels, "Healthy")
-		resultSeries = append(resultSeries, totalGood)
-		resultColors = append(resultColors, "#81C784")
+		resultSeries = append(resultSeries, totalHealthy)
+		resultColors = append(resultColors, colHealthy)
+	}
+	if totalPending > 0 {
+		resultLabels = append(resultLabels, "Pending")
+		resultSeries = append(resultSeries, totalPending)
+		resultColors = append(resultColors, colPending)
 	}
 
 	result := FinalPieChart{
-		Total:  totalBad + totalGood,
+		Total:  totalDown + totalHealthy + totalPending,
 		Series: resultSeries,
 		ChartOpts: chart.Opts{
 			Legend: chart.Legend{Show: true},
@@ -110,14 +126,58 @@ func AssembleDeploymentPieChart(
 
 // isReady is a meta kind of job
 // isReady checks if a pod has a status condition of "Available==True"
-// TODO: possibly check for "Progressing" also?
-func isReady(deployment v1.Deployment) bool {
-	for _, obj := range deployment.Status.Conditions {
-		if strings.Contains(string(obj.Type), "Available") {
-			if strings.Contains(string(obj.Status), "True") {
+func isReady(obj v1.Deployment) bool {
+	for _, condition := range obj.Status.Conditions {
+		if strings.Contains(string(condition.Type), "Available") {
+			if strings.Contains(string(condition.Status), "True") {
 				return true
 			}
 		}
 	}
 	return false
+}
+
+func isProgressing(k8sObject v1.Deployment) bool {
+	for _, obj := range k8sObject.Status.Conditions {
+		if strings.Contains(string(obj.Type), "Progressing") {
+			if strings.Contains(string(obj.Status), "True") {
+				return true
+			}
+		}
+	}
+	return true
+}
+
+func isPending(obj v1.Deployment) (result bool) {
+	// at least one pod still working but stuff is looking bad
+	if obj.Status.ReadyReplicas > 0 {
+		if obj.Status.AvailableReplicas > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func isHealthy(obj v1.Deployment) (result bool) {
+	// This is the most intense check.. basically if there is anything wrong, the object is not healthy
+	if !isReady(obj) {
+		return false
+	}
+	if !isProgressing(obj) {
+		return false
+	}
+	// we want all of these to match replicas for the deployment to be classed as healthy
+	//  updatedReplicas
+	//  readyReplicas
+	//  availableReplicas
+	if obj.Status.UpdatedReplicas != obj.Status.Replicas {
+		return false
+	}
+	if obj.Status.ReadyReplicas != obj.Status.Replicas {
+		return false
+	}
+	if obj.Status.AvailableReplicas != obj.Status.Replicas {
+		return false
+	}
+	return true
 }
